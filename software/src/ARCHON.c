cf1d478ceffa37f921c067ad6a6837e70b1283b2
Patrick Dunham <patrick.dunham.113@gmail.com>
2018-06-15 00:06:36 -0400

    Add initialization code, millis timer

--- software/src/ARCHON.c
+++ software/src/ARCHON.c
@@ -1,5 +1,6 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
+#include <stdint.h>
 
 #include "COMM.h"
 #include "UART.h"
@@ -7,50 +8,53 @@
 
 /*** CONFIGURATION ***/
 
-#define BAUD_RATE 9600UL
+#define BAUD_RATE 9600UL // [baud]
+#define LED_UPDATE_PERIOD 500 // [ms]
 
 
 /*** VARIABLES ***/
 
-COM_Message message;
+// Private
+static volatile uint32_t _millis;
 
+// Public
+COM_Message message = {.type = COM_PKT_TEST, .length = 5, .data = "INIT\n"};
+uint32_t last_led_update;
 
 /*** FUNCTION DECLARATIONS ***/
+
+void init_all(void);
+
+uint32_t millis(void);
+
+void tmr_millis_init(void);
+void tmr_millis_start(void);
+void tmr_millis_stop(void);
 
 
 /*** BODY ***/
 
 int main(void) {
-  // Setup
-  DDRB |= (1 << PINB5);
-  PORTB |= (1 << PINB5);
-  urt_init(BAUD_RATE);
+  init_all();
   sei();
 
-  // Generate test message
-  message.type = COM_PKT_TEST;
-  message.data[0] = 'D';
-  message.data[1] = 'E';
-  message.data[2] = 'A';
-  message.data[3] = 'D';
-  message.data[4] = 'B';
-  message.data[5] = 'E';
-  message.data[6] = 'E';
-  message.data[7] = 'F';
-  message.data[8] = '_';
-  message.data[9] = 'F';
-  message.data[10] = 'O';
-  message.data[11] = 'O';
-  message.data[12] = 'B';
-  message.data[13] = 'A';
-  message.data[14] = 'R';
-  message.length = 15;
+  // Start 1ms timer
+  tmr_millis_start();
+
+  com_send_packet(message);
+
+  // Configure pins
+  DDRB |= (1 << PINB5);
+  PORTB |= (1 << PINB5);
 
   while(1) {
-    // Loop
-    _delay_ms(1000);
-    PORTB ^= (1 << PINB5);
-    com_send_packet(message);
+    uint32_t _now = millis();
+
+    if (_now - last_led_update >= LED_UPDATE_PERIOD) {
+      last_led_update = _now;
+      PORTB ^= (1 << PINB5);
+      com_send_packet(message);
+    }
   }
 
   return 0;
@@ -58,3 +62,74 @@ int main(void) {
 
 
 /*** FUNCTION DEFINITIONS ***/
+
+/** @brief Initialize all modules
+ *
+ * All initialization functions are grouped here. Interrupts are *not* enabled
+ * in this function.
+ */
+void init_all(void) {
+  tmr_millis_init();
+  urt_init(BAUD_RATE);
+}
+
+/** @brief Retrieve number of milliseconds since start
+ *
+ * Wrapper for atomic access to _millis.
+ *
+ * NOTE: Will overflow approx. once every 40 days.
+ *
+ * @returns milliseconds since start
+ */
+uint32_t millis(void) {
+  uint32_t _millis_buffer;
+
+  // Store current interrupt status before disabling.
+  uint8_t _sreg = SREG;
+  cli();
+  _millis_buffer = (uint32_t)_millis;
+  SREG = _sreg;
+
+  return _millis_buffer;
+}
+
+/** @brief Initialize TIMER0 as a 1-millisecond timer
+ *
+ * Number of milliseconds since start are tracked in `_millis`, an unsigned
+ * 32-bit integer. Use `millis()` to access value, as it ensures atomic access
+ * and will cast away the `volatile` tag.
+ *
+ * Timer is placed in CTC mode and configured to trigger the Compare Match A
+ * interrupt every millisecond.
+ *
+ * NOTE: Any previous configuration of TIMER0 will be overwritten.
+ * WARNING: OCR0A and `timer_millis_start()` will need to change if F_CPU is
+ *          not 16 MHz.
+ */
+void tmr_millis_init(void) {
+  TCCR0A = (1 << WGM01);
+  TCCR0B = 0;
+  TIMSK0 = (1 << OCIE0A);
+  OCR0A = 249;
+}
+
+/** @brief Start millisecond timer (TIMER0)
+ *
+ * Currently configured for a /64 prescaler
+ *
+ * WARNING: OCR0A and `timer_millis_start()` will need to change if F_CPU is
+ *          not 16 MHz.
+ */
+void tmr_millis_start(void) {
+  TCCR0B = (1 << CS01) | (1 << CS00);
+}
+
+/// @brief Stop millisecond timer (TIMER0)
+void tmr_millis_stop(void) {
+  TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00));
+}
+
+/// @brief TIMER0 millis timer interrupt
+ISR(TIMER0_COMPA_vect) {
+  _millis++;
+}
